0b550c01cfa206486502fec9be7e5038f6f7bbb4
yuto90 <solidusgako4087@gmail.com>
2023-05-23 23:06:20 +0900

    Todoコンポーネント作成

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
 
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "~/server/api/root";
+
+// API の型定義である AppRouter の出力の型を推論するためのヘルパータイプ 
+type RouterOutput = inferRouterOutputs<AppRouter>;
+// Todo を全件取得するルーターの出力の型を定義 
+// ["todo"] の todo は、src/server/api/root.ts で定義した createTRPCRouter で指定したキー
+// ["all"] の all は、src/server/api/routers/todo.ts で定義したプロシージャの名前
+type allTodosOutput = RouterOutput["todo"]["all"];
+export type Todo = allTodosOutput[number];
+
 // zodによるバリデーションの定義
 export const createInput = z
     .string()
