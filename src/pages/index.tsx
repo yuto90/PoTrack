a023f43f71e60e9e9ebc42540948413b88850216
yuto90 <solidusgako4087@gmail.com>
2023-06-04 01:30:07 +0900

    Headを共通化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,6 @@ import TplIndex from "~/components/templates/TplIndex";
 export default function Index() {
   return (
     <>
-      <Head>
-        <title>PoTrack</title>
-        <meta name="description" content="Full stack todo app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <TplIndex />
     </>
   );
