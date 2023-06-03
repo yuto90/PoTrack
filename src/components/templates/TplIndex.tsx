cf9152ff7f8b62d7d28f9c6937f1f727fdef082a
yuto90 <solidusgako4087@gmail.com>
2023-06-04 01:12:17 +0900

    アトミックデザイン（仮）

---
+++ src/components/templates/TplIndex.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { CreateTodo } from "~/components/CreateTodo";
+import { Todos } from "~/components/Todos";
+import { isDevelopment } from "~/env.mjs";
+import { Stopwatch } from "~/components/StopWatch";
+
+export default function TplIndex() {
+    // 認証情報を取得
+    // sessionDataには認証済であれば認証情報が、認証されていなければｎullが格納される
+    // status という変数には、認証情報の取得状況・認証済みか否かという情報が格納される
+    // loading（認証情報の取得中）, unauthenticated（非認証）, authenticated（認証済み） のいずれかが格納される
+    const { data: sessionData, status } = useSession();
+
+    const mountPicture = () => {
+        const devPicture = "/images/main-dev.png";
+        const prodPicture = "/images/main.png";
+        return isDevelopment() ? devPicture : prodPicture;
+    }
+
+    return (
+        <>
+            <div className="min-h-screen bg-gray-four p-0 selection:bg-green-two md:py-24 md:px-8">
+                <main className="mx-auto min-h-screen max-w-none rounded-none bg-gray-four px-5 pt-24 pb-10 outline-none md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-high-green">
+                    {/* status が "loading" でない、つまり認証情報の取得が完了している、
+                     かつ、認証されている場合に、下記が表示される */}
+                    {status !== "loading" && sessionData && (
+                        <>
+                            <div className="flex flex-col items-center">
+                                <p className="text-l text-white mb-4 text-center text-high-green">
+                                    <span>Logged in as {sessionData.user?.name}</span>
+                                </p>
+                                <button
+                                    className="mb-8 inline-flex cursor-pointer items-center justify-center rounded-md py-2 px-4 text-high-green hover:text-gray-four font-semibold bg-gray-four hover:bg-high-green outline outline-2 outline-offset-2 outline-high-green"
+                                    onClick={() => void signOut()}
+                                >
+                                    Sign out
+                                </button>
+                            </div>
+                            <div>
+                                <div className="flex flex-row justify-around">
+                                    <CreateTodo />
+                                    <Stopwatch />
+                                </div>
+                                <Todos />
+                            </div>
+                        </>
+                    )}
+                    {status !== "loading" && !sessionData && (
+                        // status が "loading" でない、つまり認証情報の取得が完了している、
+                        // かつ、認証されていない場合に、下記が表示されます
+                        <>
+                            <h1 className="mb-6 text-center text-4xl font-bold text-high-green">
+                                PoTrack
+                            </h1>
+                            <div className="flex flex-col items-center">
+                                <button
+                                    className="mb-5 inline-flex cursor-pointer items-center justify-center rounded-md py-2 px-4 text-high-green hover:text-gray-four font-semibold bg-gray-four hover:bg-high-green outline outline-2 outline-offset-2 outline-high-green"
+                                    onClick={() => void signIn()}
+                                >
+                                    Sign In
+                                </button>
+                                <div className="mb-5 text-xl">
+                                    <p className="text-center text-gray-four">
+                                        Todo App made in T3 Stack
+                                    </p>
+                                </div>
+                                <div className="">
+                                    <Image
+                                        //src="/images/main-img.png"
+                                        src={mountPicture()}
+                                        width={400}
+                                        height={400}
+                                        alt="main-img"
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </main>
+            </div>
+        </>
+    );
+}
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,6 +1,7 @@
 import type { InferGetServerSidePropsType } from "next";
 import { getProviders, signIn } from "next-auth/react";
 import Image from "next/image";
+import TplSignIn from "~/components/templates/TplSignin";
 
 export default function SignIn({
     // ここで providers の 型を定義しています
@@ -8,37 +9,7 @@ export default function SignIn({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <>
-            <div className="min-h-screen bg-gray-four p-0 selection:bg-green-two md:py-24 md:px-8">
-                <div className="flex flex-col items-center space-y-3 pt-40">
-                    <Image
-                        src="/images/github-icon.png"
-                        width={130}
-                        height={130}
-                        alt="github-icon"
-                    />
-                    <div className="text-center">
-                        <div className="mx-auto max-w-3xl">
-                            <div className="flexjustify-center"></div>
-                            {Object.values(providers).map((provider) => (
-                                <div key={provider.name}>
-                                    <button
-                                        className="inline-flex w-full cursor-pointer items-center justify-center rounded-md p-4 text-xl font-bold hover:text-high-green"
-                                        // このボタンを押すと GitHub による認証が行われます
-                                        // また、認証後のリダイレクト先をルートパスに設定しています
-                                        onClick={() =>
-                                            void signIn(provider.id, {
-                                                callbackUrl: "/",
-                                            })
-                                        }
-                                    >
-                                        Sign in with {provider.name}
-                                    </button>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <TplSignIn providers={providers} />
         </>
     );
 }
